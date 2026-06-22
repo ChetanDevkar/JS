f39f8088845fa5f2a6971ddb505db65ca42d8baa
Chetan Vinod Devkar <chetandevkar.engg@gmail.com>
2026-06-22 05:32:45 +0000

    add

---
+++ basic/2_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treate all js code as newer version
+
+// alert("hello") we are using node.js not browser
+
+// datatypes
+let name="chetan"
+let age = 20
+let isloggedin=false
+
+// number => 2 
+//bigint
+// string => ""
+//boolean => true , false
+// null ==> standalon value
+// undefined => value is not assign
+// symbol =>unique 
+
+
+//object
+
+console.log(typeof "chetan");
+console.log(typeof age)
+
+
+console.log(typeof null) // object
+
