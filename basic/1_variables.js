51c9a50208e54e8d01c37ba051baa3acd97cb544
Chetan Vinod Devkar <chetandevkar.engg@gmail.com>
2026-06-22 04:54:13 +0000

    add file

---
+++ basic/1_variables.js
@@ -0,0 +1,9 @@
+const account_id = 145
+let account_email ="chetandevkar@gmail.com"
+var account_password ="12345"
+accountCity = "nashik"
+
+account_id = 2
+
+console.log(account_id);
+ 
--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,8 @@
 console.log("hello Chetan")
+
+//console.log(4+5)
+
+console.log("we ar starting the new journey")
+
+console.log("So lets Start") 
+console.log("hi")
