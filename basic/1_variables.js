74ba45ebefd4ab733fcec145c7def663a1894b86
Chetan Vinod Devkar <chetandevkar.engg@gmail.com>
2026-06-22 05:05:47 +0000

    1_variables done

--- basic/1_variables.js
+++ basic/1_variables.js
@@ -3,7 +3,18 @@ let account_email ="chetandevkar@gmail.com"
 var account_password ="12345"
 accountCity = "nashik"
 
-account_id = 2
+//account_id = 2 not allowed
+
+account_email="cd@gmail.com"
+account_password="145"
+accountCity="mumbai"
+let accountState;
 
 console.log(account_id);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([account_email,account_id,account_password,accountCity,accountState])
  
